076a17a980a2863d4be382e6082662b7e893b751
rayk <mrraykong@gmail.com>
2023-08-17 23:36:23 +0800

    added table and functional buttons
    Co-authored-by: derquan.augustin26@compscihigh.org <derquan.augustin26@compscihigh.org>

--- main.js
+++ main.js
@@ -1,18 +1,61 @@
-const submitButton = document.getElementById("submit-button");
-let searchResult = document.querySelector("target");
+// const tableButton = document.getElementById("table_button");
 
-submitButton.addEventListener("click", (event) => {
-    event.preventDefault();
+// tableButton.addEventListener("click", (event) => {
+//     event.preventDefault();
 
-    const inputElement = document.getElementById("boxtext");
-    const userInput = inputElement.value;
-    searchResult = userInput;
-    alert(userInput);
-    inputElement.value = "";
+//     var tableRow = document.getElementById("table");
+//     var row = document.createElement("tr");
+//     var cell1 = document.createElement("td");
+//     var cell2 = document.createElement("td");
+//     var cell3 = document.createElement("td");
+
+//     cell1.style.marginLeft = "auto";
+//     cell1.style.marginRight = "auto";
+//     cell1.style.border = "1px solid black";
+//     cell1.style.borderCollapse = "collapse";
+//     cell1.style.width = "1000px";
+//     cell1.style.height = "500px";
+
+//     cell2.style.marginLeft = "auto";
+//     cell2.style.marginRight = "auto";
+//     cell2.style.border = "1px solid black";
+//     cell2.style.borderCollapse = "collapse";
+//     cell2.style.width = "1000px";
+//     cell2.style.height = "500px";
+
+//     cell3.style.marginLeft = "auto";
+//     cell3.style.marginRight = "auto";
+//     cell3.style.border = "1px solid black";
+//     cell3.style.borderCollapse = "collapse";
+//     cell3.style.width = "1000px";
+//     cell3.style.height = "500px";
+
+//     row.appendChild(cell1);
+//     row.appendChild(cell2);
+//     row.appendChild(cell3);
+//     tableRow.appendChild(row);
+// });
+//Adds a click listener to the add-row button
+document.querySelector("#table_button").addEventListener("click", () => {
+//calls the addRow() method on clicking the button
+addRow();
 });
 
-const SEARCH_API_URL = "https://www.googleapis.com/customsearch/v1";
-const API_KEY = "AIzaSyBY1qgqkOZzHYXsEM45f0OVAwI0A-R8LmY";
-const SEARCH_ENGINE_ID = "178866f9a1b18489c";
+const addRow = () => {
+let row = document.createElement("tr");
 
-let url = new URL(SEARCH_API_URL + "?key=" + API_KEY + "&cx=" + SEARCH_ENGINE_ID + "&q=" + userInput);
+//creates a new column element
+let column1 = document.createElement("td");
+let column2 = document.createElement("td");
+let column3 = document.createElement("td");
+
+column1.contentEditable = true;
+column2.contentEditable = true;
+column3.contentEditable = true;
+
+row.appendChild(column1);
+row.appendChild(column2);
+row.appendChild(column3);
+
+document.querySelector("table").appendChild(row);
+};
